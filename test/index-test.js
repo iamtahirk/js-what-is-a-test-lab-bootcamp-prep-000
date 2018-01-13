c0817b307025ed9b147f4a44a478affffceef137
Muhammad Tahir Khan <muhammadtahirk@gmail.com>
2018-01-13 09:18:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,23 @@
-var index = require("../index.js");
+var index = require("../index.js")
 
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan');
-    });
-  });
+      expect(index.name).toEqual('Susan')
+    })
+  })
 
 
   describe('Height', () => {
-    it('is less than 800', () => {
-      expect(index.height).toBeLessThan(800);
-    });
-  });
+    it('is less than 80', () => {
+      expect(index.height).toBeLessThan(80)
+    })
+  })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name);
-      expect(index.message).toInclude(index.height);
-    });
-  });
-
-});
-
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
+    })
+  })
+})
